eaf6cabfb50648fee588cafad6787424e8e64d5c
SeonJungKim <ksj083895@gmail.com>
2021-02-01 18:33:16 +0500

    Initial Commit

---
+++ SwfitUI_Picker,ForEach,Section_TUT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwfitUI_Picker,ForEach,Section_TUT
+//
+//  Created by 김선중 on 2021/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
