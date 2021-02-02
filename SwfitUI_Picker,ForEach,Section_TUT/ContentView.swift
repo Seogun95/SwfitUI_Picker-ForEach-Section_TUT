e30419e565359e5b684578c7ecb52925e1775cc5
SeonJungKim <ksj083895@gmail.com>
2021-02-02 12:05:26 +0500

    Picker, ForEach, Section

--- SwfitUI_Picker,ForEach,Section_TUT/ContentView.swift
+++ SwfitUI_Picker,ForEach,Section_TUT/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmound = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 0
+    
+    //퍼센트 단위를 딕셔너리로 만들어주고
+    let tipPercentages = [10, 15, 20, 25 , 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+               //텍스트필드 섹션
+               Section {
+                    TextField("Amount", text: $checkAmound)
+                        .keyboardType(.decimalPad)
+                    
+                    //피커 섹션
+                    Picker("number of people", selection: $numberOfPeople) {
+                        ForEach(0 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                //퍼센트 섹션
+                Section(header: Text("How much tip do you want leave")) {
+                    Picker("tip Perentage", selection: $tipPercentage) {
+ 
+                        //0..< = 딕셔너리의 [10]부터 tipPercentage보다 작은값
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .textCase(nil)
+ 
+                Section {
+                    Text("$ \(checkAmound)")
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
